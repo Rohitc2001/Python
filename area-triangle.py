ea6323d480322d05c525eaf7d7de207e5b6f2bde
Rohit Chaudhary <63979426+Rohitc2001@users.noreply.github.com>
2021-04-02 23:10:36 +0530

    Find area of Triangle.

---
+++ area-triangle.py
@@ -0,0 +1,9 @@
+def area(l,h):
+    result = 1/2 * l * h
+
+    return result
+
+l = float(input("Entern the length : "))
+h = float(input("Entern the height : "))
+
+print("Area of Triangle is ",area(l,h))
