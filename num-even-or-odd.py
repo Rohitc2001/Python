ccb10eac705e908050aae816809eaf96584f24cd
Rohit Chaudhary <63979426+Rohitc2001@users.noreply.github.com>
2021-04-05 00:16:00 +0530

    Check entered number is even or odd.

---
+++ num-even-or-odd.py
@@ -0,0 +1,8 @@
+									#program to check number is even or odd.
+												
+n = int(input("Enter the number : "))
+
+if n%2 == 0 :
+    print("Number is even ")
+else :
+    print("Number is odd ")
