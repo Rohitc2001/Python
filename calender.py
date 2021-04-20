763a59bb83f9aa6bfe7d3c82688b9ccbaa665d12
Rohit Chaudhary <63979426+Rohitc2001@users.noreply.github.com>
2021-04-20 23:40:10 +0530

    Print Calender of entered Month and Year

---
+++ calender.py
@@ -0,0 +1,7 @@
+                # Program to print the Calender of entered Month & Year 
+import calendar
+
+m = int(input("Enter Month Number: "))
+y = int(input("Enter Year Number: "))
+
+print(calendar.month(y,m))
